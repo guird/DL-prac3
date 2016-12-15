df71dcaa5ae62d9c9fa482e81d7317285f16f95f
The Hug <guirdian4@gmail.com>
2016-12-15 21:03:58 +0100

    problem only showing up on cartesius now + import sys

--- cifar10_siamese_utils.py
+++ cifar10_siamese_utils.py
@@ -5,7 +5,7 @@ You don't need to change anything here.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import sys
 import numpy as np
 import os
 import cPickle as pickle
